6d0e208ce1f73a61a25fca3cfa42d2b3c2d5be06
zeref1428 <85298883+zeref1428@users.noreply.github.com>
2021-12-07 01:11:47 +0530

    Add files via upload

---
+++ Binary Tree/Tree_Level_Order_Traversal(Easy).cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data =val;
+        left=NULL;
+        right=NULL;
+    }
+};
+struct solution{
+    Node* insert(Node* root, int data){
+        if(root==NULL){
+        return new Node(data);
+    }
+    else{
+        Node* cur;
+        if(data<=root->data){
+            cur=insert(root->left,data);
+            root->left=cur;
+        }
+        else{
+            cur=insert(root->right,data);
+            root->right=cur;
+        }
+        return root;
+    }
+}
+};
+int main(){
+    solution mytree;
+    Node* root=NULL;
+    int t,data;
+    cin>>t;
+    while(t--){
+        cin>>data;
+        root=mytree.insert(root,data);
+        cout<<data<<" ";
+    };
+    return 0;
+}
